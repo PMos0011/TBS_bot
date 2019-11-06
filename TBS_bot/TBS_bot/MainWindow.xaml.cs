eaad7a1e2a0575ee383fc712d0d60465e341d0bc
Paweł <pawel.moskwa.pm@gmail.com>
2019-11-06 22:15:51 +0100

    Downloading basic informations from web

---
+++ TBS_bot/TBS_bot/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TBS_bot
+{
+
+    public partial class MainWindow : Window
+    {
+
+        List<string> links = new List<string>();
+        List<string> addresses = new List<string>();
+        List<string> FlatDescription = new List<string>();
+        List<string> flatDescriptionReplacementList = new List<string>
+        {
+            "<li>",
+            "</li>",
+            "<sup>",
+            "</sup>",
+            " &nbsp;",
+            "&nbsp;",
+            "\t",
+        };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetFlatsList_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Cursor = Cursors.Wait;
+            addressesTB.Items.Clear();
+
+            string Result = await GetPageString("http://www.tbs-wroclaw.com.pl/mieszkania-na-wynajem/");
+            GetFlatsList(Result);
+
+            foreach (var item in links)
+            {
+                Result = await GetPageString(item);
+                FlatDescription.Add(getFlatDescription(Result));
+            }
+            mainWindow.Cursor = Cursors.Arrow;
+        }
+
+        private async Task<string> GetPageString(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            string Result = await httpClient.GetStringAsync(url);
+            return Result;
+        }
+
+        private void GetFlatsList(string Result)
+        {
+            Result = Result.Substring(Result.IndexOf("<p><strong><a href=\""));
+            Result = Result.Substring(0, Result.IndexOf("<p>&nbsp;</p>"));
+            string[] Paragraphs = Result.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in Paragraphs)
+            {
+                string line = item.Replace("<strong>", "");
+                line = line.Replace("</strong>", "");
+                string TemporaryItem = line.Substring(line.IndexOf('"') + 1);
+                TemporaryItem = TemporaryItem.Substring(0, TemporaryItem.IndexOf('"'));
+                links.Add(TemporaryItem);
+                TemporaryItem = line.Substring(line.IndexOf("\">") + 2);
+                TemporaryItem = TemporaryItem.Substring(0, TemporaryItem.IndexOf("</"));
+                addresses.Add(TemporaryItem);
+            }
+
+            foreach (var item in addresses)
+            {
+                addressesTB.Items.Add(item);
+            }
+        }
+
+        private string getFlatDescription(string Result)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Result = Result.Substring(Result.IndexOf("(osiedle")+1);
+
+            stringBuilder.Append(Result.Substring(0, Result.IndexOf(")")) + Environment.NewLine);
+            Result = Result.Substring(Result.IndexOf("o powierzchni") + 14);
+
+            double flatArea = Convert.ToDouble(Regex.Replace(Result.Substring(0, Result.IndexOf("składający się") - 2), "[^0-9,]", ""));
+
+            stringBuilder.Append("pow: " + flatArea + Environment.NewLine);
+
+            Result = Result.Substring(Result.IndexOf("<ol>")+4);
+            Result = Result.Substring(0, Result.IndexOf("</ol>"));
+
+            foreach (var item in flatDescriptionReplacementList)
+            {
+                Result = Result.Replace(item, "");
+            }
+ 
+            stringBuilder.Append(Result + Environment.NewLine);
+            stringBuilder.Append("part: " + (flatArea*1200).ToString("F") + Environment.NewLine);
+            stringBuilder.Append("czynsz: " + (flatArea * 14.25).ToString("F") + Environment.NewLine);
+
+
+            return stringBuilder.ToString();
+        }
+
+        private void AddressesTB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            linksTB.Text = FlatDescription.ElementAt(addressesTB.SelectedIndex);
+            Hyperlink hyperlink = new Hyperlink();
+            hyperlink.Inlines.Add(links.ElementAt(addressesTB.SelectedIndex));
+            hyperlink.Click += new RoutedEventHandler(HyperLinkClick);
+            hyperLinkTB.Text = "";
+            hyperLinkTB.Inlines.Add(hyperlink);
+        }
+
+        private void HyperLinkClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start(hyperLinkTB.Text);
+        }
+
+    }
+}
